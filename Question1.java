eae0f64a33585f50ba97667174424c407cd71249
nayeem <52297907+nayeem-bd@users.noreply.github.com>
2022-01-03 19:19:27 +0600

    Add files via upload

---
+++ Question1.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+//    Convert Integer to Roman
+
+public class Question1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] romanL = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        String showRoman = new String();
+        for(int i=0;i<values.length;i++){
+            while(num >= values[i]){
+                num-=values[i];
+                showRoman+=romanL[i];
+            }
+        }
+        System.out.println(showRoman);
+    }
+}
