580988fffafb52cb211c2f692acf23710fd6d557
nayeem <52297907+nayeem-bd@users.noreply.github.com>
2022-01-04 20:44:28 +0600

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -1,33 +1,38 @@
 //convert an integer to word
+
 import java.util.Scanner;
 
-
 public class Question2 {
-    public static final String[] units = {"", "one", "two", "three", "four", "five", "six", "seven","eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen","fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
-    public static final String[] tens = {"","","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
-    
-    public static String convert(int n){
-        if(n<20){
-            return units[n];
-        }
-        if(n<100){
-            return tens[n/10]+((n%10!=0)?" ":"")+units[n%10];
-        }
-        if(n<1000){
-            return units[n/100] + " hundred" + ((n%100!=0)?" ":"") + convert(n%100);
-        } 
-        if(n<100000){
-            return convert(n/1000) + " thousand"+((n%1000!=0)?" ":"") +convert(n%1000);
-        }  
-        return null;
-    }
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int num = input.nextInt();
 
-        String word = convert(num);
+    public static void main(String[] args) {
+        String[] units = {"", "one", "two", "three", "four", "five", "six", "seven",
+            "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+            "eighteen", "nineteen"};
+        String[] tens = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        String word = new String();
+        while (n > 0) {
+            if (n < 20) {
+                word += units[n];
+                break;
+            } 
+            else if (n < 100) {
+                word += tens[n / 10] + " ";
+                n %= 10;
+            } 
+            else if (n < 1000) {
+                word += units[n / 100] + " hundred ";
+                n %= 100;
+            } 
+            else if (n < 10000) {
+                word += units[n / 1000] + " thousand ";
+                n %= 1000;
+            }
+        }
         System.out.println(word);
-        
 
     }
 }
