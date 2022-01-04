834a514440ffe08ad9661db027191a88caaec79e
nayeem <52297907+nayeem-bd@users.noreply.github.com>
2022-01-04 20:47:44 +0600

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -1,12 +1,13 @@
-//    Convert Integer to Roman
 import java.util.Scanner;
+
+//    Convert Integer to Roman
 
 public class Question1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int num = input.nextInt();
-        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        String[] romanL = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int[] values = {10000,9000,5000,4000,1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] romanL = {"H","MH","G","MG","M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         String showRoman = new String();
         for(int i=0;i<values.length;i++){
             while(num >= values[i]){
